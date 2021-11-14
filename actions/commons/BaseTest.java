00cccf743a085c8e47cb7167c7168ac54227bd44
phucan2410 <nguyenphucan24101996@gmail.com>
2021-11-15 00:46:57 +0700

    Multi browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,29 @@
 package commons;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+public class BaseTest{
+	private WebDriver driverBaseTest;
+	private String projectPath = System.getProperty("user.dir");
+	
+	protected WebDriver getBrowserDriver(String browserName) {
+	
+		if(browserName.equals("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			driverBaseTest = new FirefoxDriver();
+		} else if (browserName.equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			driverBaseTest = new ChromeDriver();
+		} else if (browserName.equals("edge")) {
+			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+			driverBaseTest = new EdgeDriver();
+		} else {
+			throw new RuntimeException("Browser name invalid.");
+		}
+		return driverBaseTest;
+
+	}
 }
